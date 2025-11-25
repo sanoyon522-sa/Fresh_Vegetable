55e7370c7b18d8033baf18f79ed6259fddb98914
SA Noyon <sanoyon522@gmail.com>
2025-11-25 18:08:23 +0600

    Add files via upload

---
+++ Figma_no_02/script.js
@@ -0,0 +1,6 @@
+let mobileMenu = document.getElementById('mobileMenu');
+let bar = document.getElementById('bar');
+
+bar.addEventListener('click', () => {
+  mobileMenu.classList.toggle('hidden');
+});
